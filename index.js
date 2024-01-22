a23a658b36b53406fbb27e51fa89865ad6130fa6
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2024-01-22 21:48:51 +1300

    Add single track to 2023 playlist

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 const { SpotifyApi } = require("@spotify/web-api-ts-sdk");
-const { differenceInMonths } = require("date-fns");
+const { differenceInMonths, getYear } = require("date-fns");
 
 const monthsDiffCutoff = 9;
+const years = {
+  2023: {
+    playlistId: "5Z0GqRR0v89ORcwCQWV4L5",
+    trackUrisToAdd: [],
+  },
+};
 
 const refreshAccessToken = async () => {
   const url = "https://accounts.spotify.com/api/token";
@@ -45,19 +51,38 @@ const getSavedTracks = async (sdk) => {
   const sdk = SpotifyApi.withAccessToken(process.env.CLIENT_ID, accessToken);
 
   const savedTracks = await getSavedTracks(sdk);
+  // const savedTracks = track;
   // console.log(savedTracks);
   // console.log(savedTracks.length);
   // console.log(savedTracks[savedTracks.length - 1]);
 
+  const now = new Date();
   const tracksToRemove = savedTracks.filter((track) => {
     const addedAt = new Date(track.added_at);
-    const now = new Date();
     const monthsDiff = differenceInMonths(now, addedAt);
     return monthsDiff >= monthsDiffCutoff;
   });
+
   tracksToRemove.reverse();
   // console.log(tracksToRemove);
   // console.log(tracksToRemove.length);
   // console.log(tracksToRemove[0]);
   // console.log(tracksToRemove[tracksToRemove.length - 1]);
+
+  const track = tracksToRemove[0];
+  // console.log(track);
+
+  const trackYear = getYear(new Date(track.added_at));
+  const year = years[trackYear];
+  year.trackUrisToAdd.push(track.track.uri);
+  // console.log(year);
+  // const playlistId = playlistIds[year];
+  // console.log(playlistId);
+  Object.values(years).forEach(({ playlistId, trackUrisToAdd }) => {
+    if (!!trackUrisToAdd.length) {
+      // console.log(playlistId);
+      // console.log(trackUrisToAdd);
+      sdk.playlists.addItemsToPlaylist(playlistId, trackUrisToAdd);
+    }
+  });
 })();
