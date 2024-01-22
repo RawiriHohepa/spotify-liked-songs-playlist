df477b3e996aee85fc45d9d55404f8bcabe82d9f
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2024-01-22 20:21:14 +1300

    Add function to generate auth code URL

---
+++ auth.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const querystring = require("node:querystring");
+var crypto = require("crypto");
+
+var id = crypto.randomBytes(20).toString("hex");
+
+var state = id.slice(0, 16);
+var scope =
+  "playlist-read-private playlist-modify-private user-library-read user-library-modify";
+
+console.log(
+  "https://accounts.spotify.com/authorize?" +
+    querystring.stringify({
+      response_type: "code",
+      client_id: process.env.CLIENT_ID,
+      scope: scope,
+      redirect_uri: process.env.REDIRECT_URI,
+      state: state,
+    })
+);
